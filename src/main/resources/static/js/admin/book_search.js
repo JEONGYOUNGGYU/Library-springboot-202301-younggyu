9e3cdad62df5d145ab0c7d74010b7ae9e70b5da4
JEONGYOUNGGYU <zkdlf90@gmail.com>
2023-01-25 21:50:45 +0900

    cors 활성, bookSearch기능 구현중 대여상태 쿼리 수정

--- src/main/resources/static/js/admin/book_search.js
+++ src/main/resources/static/js/admin/book_search.js
@@ -1,5 +1,7 @@
 window.onload = () => {
-
+    BookService.getInstance().loadBookList();
+    BookService.getInstance().loadCategories();
+    ComponentEvent.getInstance().addClickEventSearchButton();
 }
 
 let searchObj = {
@@ -26,17 +28,39 @@ class BookSearchApi {
         $.ajax({
             async: false,
             type: "get",
-            utl: "http://127.0.0.1:8000/api/admin/books",
+            url: "http://127.0.0.1:8000/api/admin/books",
+            data: searchObj,
             dataType: "json",
             success: response => { //CMResp의 데이터가 response.data로 들어온다
+                console.log(response);
                 returnData = response.data;
             },
             error: error => {
                 console.log(error);
             }
-        })
+        });
+        
+        return returnData;
     }
+    getCategories() {
+        let returnData = null;
 
+        $.ajax({
+            async: false,
+            type: "get",
+            url: "http://127.0.0.1:8000/api/admin/categories",
+            dataType: "json",
+            success: response => { //CMResp의 데이터가 response.data로 들어온다
+                console.log(response);
+                returnData = response.data;
+            },
+            error: error => {
+                console.log(error);
+            }
+        });
+
+        return returnData;
+    }
 }
 
 class BookService {
@@ -47,7 +71,72 @@ class BookService {
         }
         return this.#instance;
     }
-    getBookList() {
 
+    loadBookList() {
+        const responseData = BookSearchApi.getInstance().getBookList(searchObj);
+    
+        const bookListBody = document.querySelector(".content-table tbody")
+        bookListBody.innerHTML = "";
+
+        responseData.forEach((data, index) => {
+            bookListBody.innerHTML += `
+                <tr>
+                    <td><INPUT type="checkbox"></INPUT></td>
+                    <td>${data.bookId}</td>
+                    <td>${data.bookCode}</td>
+                    <td>${data.bookName}</td>
+                    <td>${data.author}</td>
+                    <td>${data.publisher}</td>
+                    <td>${data.publicationDate}</td>
+                    <td>${data.category}</td>
+                    <td>${data.rentalStatus == "Y" ? "대여중" : "대여가능"}</td>
+                    <td><i class="fa-solid fa-square-pen"></i></td>
+                    <td><i class="fa-solid fa-square-minus"></i></td>
+                </tr>
+            `;
+        });
+    }
+
+    loadCategories() {
+        const responseData = BookSearchApi.getInstance().getCategories();
+
+
+        const categorySelect = document.querySelector(".category-select");
+        categorySelect.innerHTML = `<option value="">전체조회</option>`;
+    
+        responseData.forEach(data => {
+            categorySelect.innerHTML += `
+                <option value="${data.category}">${data.category}</option>
+            `;
+        });
+        
+    }
+}
+class ComponentEvent {
+    static #instance = null;
+    static getInstance() {
+        if(this.#instance == null){
+            this.#instance = new ComponentEvent();
+        }
+        return this.#instance;
+    }
+
+    addClickEventSearchButton(){
+        const categorySelect = document.querySelector(".category-select")
+        const searchInput = document.querySelector(".search-input");
+        const searchButton = document.querySelector(".search-button");
+
+        searchButton.onclick = () => {
+            searchObj.category = categorySelect.value;
+            searchObj.searchValue = searchInput.value;
+            
+            BookService.getInstance().loadBookList();   // bookservice 안에 있는 loadbooklist()를 호출하면...
+        }
+
+        searchInput.onkeyup = () => {
+            if(window.event.keyCode == 13) {
+                searchButton.click();
+            }
+        }
     }
 }
